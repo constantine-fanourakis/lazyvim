6d8159167bc6de3a4e70aa3cce49375201795a25
Constantine Fanourakis <constantine@inspiren.com>
2025-02-04 17:03:25 -0500

    feat: more customization

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,6 @@ vim.g.snacks_animate = false
 vim.g.snacks_indent = false
 
 vim.g.autoformat = false
+
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "basedpyright"
