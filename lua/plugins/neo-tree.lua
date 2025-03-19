615c494ccc3c673e5c65eb4338df1c8afd187fc8
Constantine Fanourakis <constantine@inspiren.com>
2025-03-19 09:41:17 -0400

    fix: make neotree follow current buffer location

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,16 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+      { "<leader>e", "<cmd>Neotree reveal float<cr>" },
+  },
   opts = {
+    sources = { "filesystem", "buffers", "git_status" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+    },
     window = {
       position = "float",
       width = 40,
@@ -11,6 +21,24 @@ return {
       mappings = {
         ["<esc>"] = "close_window",
         ["q"] = "close_window",
+        ["l"] = "open",
+        ["h"] = "close_node",
+        ["<space>"] = "none",
+        ["Y"] = {
+            function(state)
+                local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg("+", path, "c")
+            end,
+            desc = "Copy Path to Clipboard",
+          },
+        ["O"] = {
+            function(state)
+                require("lazy.util").open(state.tree:get_node().path, { system = true })
+            end,
+            desc = "Open with System Application",
+          },
+        ["P"] = { "toggle_preview", config = { use_float = false } },
       },
     },
     popup_border_style = "rounded",
